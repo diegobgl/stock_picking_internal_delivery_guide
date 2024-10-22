ab1b50f5d774a90867873beef848b8177b2c274d
Diego Gajardo <deskodoo@a2group.cl>
2024-10-22 14:16:08 -0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+
+{
+    'name': 'Stock Picking Internal Delivery Guide',
+    'version': '1.0',
+    'summary': 'Muestra el botón de Create Delivery Guide en transferencias internas.',
+    'author': 'Tu nombre',
+    'depends': ['stock', 'l10n_cl'],
+    'data': [
+        'views/stock_picking_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
